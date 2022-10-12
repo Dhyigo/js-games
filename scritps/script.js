74886392e4c6ef568b6ef1b1e6973ef584786bed
Diego Silva <aldhiego666@gmail.com>
2022-10-12 12:58:13 -0300

    Refatoração

--- scritps/script.js
+++ scritps/script.js
@@ -1,86 +1,85 @@
-let abrir = true
-let tema = 'claro'
-let html = document.querySelector('html')
-let cssVariavel = ['--cor1', '--cor2', '--cor3', '--cor4', '--cor4m', '--cor5']
-let temaEscuro = ['#121212', '#181818', '#202020', '#4B4B4B', '#b8b7b7', '#FFFFFF']
-let temaClaro = ['#FFFFFF', '#F9F9F9', '#E5E5E5', '#b6b6b6', '#636363', '#030303']
+function myScope() {    
+    let abrir = true
+    let tema = 'claro'
+    const html = document.querySelector('html')
+    const cssVariavel = ['--cor1', '--cor2', '--cor3', '--cor4', '--cor4m', '--cor5']
+    const temaEscuro = ['#121212', '#181818', '#202020', '#4B4B4B', '#b8b7b7', '#FFFFFF']
+    const temaClaro = ['#FFFFFF', '#F9F9F9', '#E5E5E5', '#b6b6b6', '#636363', '#030303']
 
-let input = document.querySelector('#caixaTxt')
-let berguer = document.querySelector('#burguer')
-let botaoLupa = document.querySelector('#lupa')
-let botaoTema = document.querySelector('#botaoTema')
+    const input = document.querySelector('#caixaTxt')
+    const berguer = document.querySelector('#burguer')
+    const botaoLupa = document.querySelector('#lupa')
+    const botaoTema = document.querySelector('#botaoTema')
 
-window.addEventListener('resize', mudouTamanho)
-berguer.addEventListener('click', mostrarMenu)
-botaoTema.addEventListener('click', mudarTema)
-botaoLupa.addEventListener('click', pesquisar)
 
-function mudouTamanho() {
-    console.log(window.innerWidth)
-    let menu = document.querySelector('ul#ulMenu')
+    window.addEventListener('resize', function () {
+        let menu = document.querySelector('ul#ulMenu')
 
-    if (window.innerWidth >= 768) {
-       menu.style.display = 'flex'
-    } else {
-        menu.style.display = 'none'
-    }
-}
+        if (window.innerWidth >= 768) {
+        menu.style.display = 'flex'
+        } else {
+            menu.style.display = 'none'
+        }
+    })
 
-// pesquisar
 
-function pesquisar() {
-    let pags = ['acao', 'aventura', 'index']
-    let pag = input.value.toLowerCase()
-    if (pag == 'casa') {
-        pag = 'index'
-    } else if (pag == 'ação') {
-        pag = 'acao'  
-    }
-    
-    if (pags.indexOf(pag) == -1) {
-        alert('[ERRO] Página não existe!')
+    berguer.addEventListener('click', function() {
+        const menu = document.getElementById('ulMenu')
+        let display = ''
 
-    } else {
-        botaoLupa.href = `./${pag}.html`
-    }
-    value.value = ''
-}
+        if (abrir) {
+            display = 'block'
+            abrir = false
+            
+        } else {
+            display = 'none'
+            abrir = true
+        }
 
-function colocarCor(lista) {
-    for (let c in cssVariavel) {
-        html.style.setProperty(cssVariavel[c], lista[c])
-    }
-}
+        menu.style.display = display
+    })
 
-function mudarTema () {
-    let imglupa = document.querySelector('img#imgLupa')
-    let imgtema = document.querySelector('img#imgTema')
 
-    if (tema == 'claro') {
-        colocarCor(temaEscuro)
-        tema = 'escuro'
-    } else {
-        colocarCor(temaClaro)
-        tema = 'claro'
-    }
+    botaoTema.addEventListener('click', function () {
+        const imglupa = document.querySelector('img#imgLupa')
+        const imgtema = document.querySelector('img#imgTema')
 
-    imglupa.src = `imagens/search-${tema}.svg`
-    imgtema.src = `imagens/tema-${tema}.svg`
+        if (tema == 'claro') {
+            colocarCor(temaEscuro)
+            tema = 'escuro'
+        } else {
+            colocarCor(temaClaro)
+            tema = 'claro'
+        }
 
-}
+        imglupa.src = `imagens/search-${tema}.svg`
+        imgtema.src = `imagens/tema-${tema}.svg`
+    })
 
-function mostrarMenu() {
-    let menu = document.getElementById('ulMenu')
-    let display = ''
 
-    if (abrir) {
-        display = 'block'
-        abrir = false
+    botaoLupa.addEventListener('click', function () {
+        const pags = ['acao', 'aventura', 'index']
+        let pag = input.value.toLowerCase()
+        if (pag == 'casa') {
+            pag = 'index'
+        } else if (pag == 'ação') {
+            pag = 'acao'  
+        }
         
-    } else {
-        display = 'none'
-        abrir = true
-    }
+        if (pags.indexOf(pag) == -1) {
+            alert('[ERRO] Página não existe!')
 
-    menu.style.display = display
+        } else {
+            botaoLupa.href = `./${pag}.html`
+        }
+    })
+
+
+    function colocarCor(lista) {
+        for (let c in cssVariavel) {
+            html.style.setProperty(cssVariavel[c], lista[c])
+        }
+    }
 }
+
+myScope()
