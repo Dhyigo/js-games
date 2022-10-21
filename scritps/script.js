82534e1017d08b0426db5ffe2cc3179f8cba96ed
Diego Silva <aldhiego666@gmail.com>
2022-10-21 20:13:57 -0300

    Refatoramento

--- scritps/script.js
+++ scritps/script.js
@@ -1,4 +1,4 @@
-function myScope() {    
+function myScope() {
     let abrir = true
     let tema = 'claro'
     const html = document.querySelector('html')
@@ -7,72 +7,75 @@ function myScope() {
     const temaClaro = ['#FFFFFF', '#F9F9F9', '#E5E5E5', '#b6b6b6', '#636363', '#030303']
 
     const input = document.querySelector('#caixaTxt')
-    const berguer = document.querySelector('#burguer')
     const botaoLupa = document.querySelector('#lupa')
-    const botaoTema = document.querySelector('#botaoTema')
 
 
     window.addEventListener('resize', function () {
         let menu = document.querySelector('ul#ulMenu')
 
         if (window.innerWidth >= 768) {
-        menu.style.display = 'flex'
+            menu.style.display = 'flex'
         } else {
             menu.style.display = 'none'
         }
     })
 
 
-    berguer.addEventListener('click', function() {
-        const menu = document.getElementById('ulMenu')
-        let display = ''
+    document.addEventListener('click', function (event) {
+        const element = event.target;
+        console.log(element, element.id);
 
-        if (abrir) {
-            display = 'block'
-            abrir = false
-            
-        } else {
-            display = 'none'
-            abrir = true
+        if (element.id === 'burguer') {
+            const menu = document.getElementById('ulMenu')
+            let display = ''
+
+            if (abrir) {
+                display = 'block'
+                abrir = false
+
+            } else {
+                display = 'none'
+                abrir = true
+            }
+
+            menu.style.display = display
         }
 
-        menu.style.display = display
-    })
+        if (element.id === 'imgTema') {
+            const imglupa = document.querySelector('img#imgLupa')
+            const imgtema = document.querySelector('img#imgTema')
 
+            if (tema == 'claro') {
+                colocarCor(temaEscuro)
+                tema = 'escuro'
 
-    botaoTema.addEventListener('click', function () {
-        const imglupa = document.querySelector('img#imgLupa')
-        const imgtema = document.querySelector('img#imgTema')
+            } else {
+                colocarCor(temaClaro)
+                tema = 'claro'
+            }
 
-        if (tema == 'claro') {
-            colocarCor(temaEscuro)
-            tema = 'escuro'
-        } else {
-            colocarCor(temaClaro)
-            tema = 'claro'
+            imglupa.src = `imagens/search-${tema}.svg`;
+            imgtema.src = `imagens/tema-${tema}.svg`;
         }
 
-        imglupa.src = `imagens/search-${tema}.svg`
-        imgtema.src = `imagens/tema-${tema}.svg`
-    })
+        if (element.id === 'imgLupa') {
+            const pags = ['acao', 'aventura', 'index']
+            let pag = input.value.toLowerCase()
 
+            if (pag == 'casa') {
+                pag = 'index'
+            } else if (pag == 'ação') {
+                pag = 'acao'
+            }
 
-    botaoLupa.addEventListener('click', function () {
-        const pags = ['acao', 'aventura', 'index']
-        let pag = input.value.toLowerCase()
-        if (pag == 'casa') {
-            pag = 'index'
-        } else if (pag == 'ação') {
-            pag = 'acao'  
+            if (pags.indexOf(pag) == -1) {
+                alert('[ERRO] Página não existe!')
+
+            } else {
+                botaoLupa.href = `./${pag}.html`
+            }
         }
-        
-        if (pags.indexOf(pag) == -1) {
-            alert('[ERRO] Página não existe!')
-
-        } else {
-            botaoLupa.href = `./${pag}.html`
-        }
-    })
+    });
 
 
     function colocarCor(lista) {
